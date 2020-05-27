119b98cb07f13cb49a1aae357f5c53b9651dd2f4
Murray Steele <contact@murto.dev>
2020-05-27 18:36:47 +0100

    Add match_results and regex_match stubs

--- include/initregex.hpp
+++ include/initregex.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -39,5 +40,41 @@ private:
 
   regex_expr expr;
 };
+
+class match_results {
+
+public:
+  match_results() = delete;
+
+  bool ready() noexcept;
+
+  bool empty() noexcept;
+
+  std::string::size_type length();
+
+  std::string str() { return *match; }
+
+private:
+  bool is_ready;
+  std::optional<std::string> match;
+};
+
+template <typename it_type>
+bool regex_match(it_type begin, it_type end, regex r) {
+  return false;
+}
+
+template <typename it_type>
+bool regex_match(it_type begin, it_type end, match_results m, regex r) {
+  return false;
+}
+
+bool regex_match(const char *s, regex r);
+
+bool regex_match(const char *s, regex r);
+
+bool regex_match(const std::string &s, regex r);
+
+bool regex_match(const std::string &s, regex r);
 
 } // namespace initregex
