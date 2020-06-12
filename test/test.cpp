5a6037737364a2bac890fdf29b8117bb2e39140d
Murray Steele <contact@murto.dev>
2020-06-12 14:02:06 +0100

    Add more sophisticated test

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,12 @@
 #include <iostream>
 
 int main() {
-  std::string input;
+  std::string pattern, input;
+  std::cout << "Enter a regular expression: ";
+  std::cin >> pattern;
+  std::cout << "Enter an input: ";
   std::cin >> input;
-  initregex::regex regex(input);
+  initregex::regex regex(pattern);
+  auto match = initregex::regex_match(input, regex);
+  std::cout << (match ? "MATCH" : "NO MATCH") << '\n';
 }
