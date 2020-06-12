6e4111fbf8cf938da907d3a9e1ff3879aa70b170
Murray Steele <contact@murto.dev>
2020-06-12 11:56:52 +0100

    Add nfa_visitor logic

--- include/initregex.hpp
+++ include/initregex.hpp
@@ -16,7 +16,11 @@ public:
     accepting_states.insert(state);
   }
   void add_transition(symbol_type symbol, int from, int to) noexcept {
-    transitions[{symbol, from}] = to;
+    transitions[{symbol, from}].insert(to);
+  }
+
+  void add_transition(int from, int to) noexcept {
+    epsilon_transitions[from].insert(to);
   }
 
 private:
@@ -32,7 +36,10 @@ private:
 
   std::unordered_set<int> initial_states;
   std::unordered_set<int> accepting_states;
-  std::unordered_map<std::pair<symbol_type, int>, int, pair_hash> transitions;
+  std::unordered_map<std::pair<symbol_type, int>, std::unordered_set<int>,
+                     pair_hash>
+      transitions;
+  std::unordered_map<int, std::unordered_set<int>> epsilon_transitions;
   std::unordered_set<int> states;
 };
 
@@ -46,10 +53,12 @@ public:
   regex(std::string &&pattern);
 
 private:
-  nfa<char> accepter;
+  nfa<char> acceptor;
 };
 
 class simple_expr {
+
+  template <typename T> friend class nfa_visitor;
 
 public:
   simple_expr(char c, bool dupl);
@@ -57,9 +66,15 @@ public:
 private:
   char c;
   bool dupl;
+
+  template <typename visitor_type> auto accept(visitor_type &visitor) const {
+    return visitor.visit(*this);
+  }
 };
 
 class regex_expr {
+
+  template <typename T> friend class nfa_visitor;
 
 public:
   regex_expr(bool l_anchor, const std::vector<simple_expr> &exprs,
@@ -69,6 +84,45 @@ private:
   bool l_anchor;
   std::vector<simple_expr> exprs;
   bool r_anchor;
+
+  template <typename visitor_type> auto accept(visitor_type &visitor) const {
+    return visitor.visit(*this);
+  }
+};
+
+template <typename symbol_type> class nfa_visitor {
+
+public:
+  nfa<symbol_type> acceptor;
+
+  nfa_visitor() { acceptor.add_initial_state(next_id()); }
+
+  void visit(const regex_expr &expr) {
+    for (const auto &simple : expr.exprs) {
+      simple.accept(*this);
+    }
+    next_id();
+    acceptor.add_accepting_state(prev_id);
+  }
+
+  void visit(const simple_expr &expr) {
+    int state_id = next_id();
+    if (expr.dupl) {
+      acceptor.add_transition(expr.c, prev_id, prev_id);
+      acceptor.add_transition(prev_id, state_id);
+    } else {
+      acceptor.add_transition(expr.c, prev_id, state_id);
+    }
+  }
+
+private:
+  int id = 0;
+  int prev_id;
+
+  int next_id() noexcept {
+    prev_id = id++;
+    return id;
+  }
 };
 
 class match_results {
